9724af21e7e8880b3d75d892d302e1826c07f35e
elnorton <160343814+elnorton@users.noreply.github.com>
2024-02-24 15:16:34 -0700

    Add files via upload
    updating

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const red = 96;
-const green = 79;
-const blue = 131;
+//const red = 96;
+// const green = 79;
+// const blue = 131;
 
-const header = document.getElementById("header");
-console.log(header.innerText);
+// const header = document.getElementById("header");
+// console.log(header.innerText);
 
-function changeColor(){
-    const scrollFactor = 1 + window.scrollY / 400;
+// function changeColor(){
+//     const scrollFactor = 1 + window.scrollY / 400;
 
-    const r = red / scrollFactor;
-    const g = green / scrollFactor;
-    const b = blue / scrollFactor;
+//     const r = red / scrollFactor;
+//     const g = green / scrollFactor;
+//     const b = blue / scrollFactor;
 
-        const updatedColor = `rgb(${r}, ${g}, ${b})`;
-        header.style.backgroundColor =updatedColor;
+//         const updatedColor = `rgb(${r}, ${g}, ${b})`;
+//         header.style.backgroundColor =updatedColor;
 
-    console.log (r, g, b);
-}
-changeColor();
-window.addEventListener('scroll', changeColor)
+//     console.log (r, g, b);
+// }
+// changeColor();
+// window.addEventListener('scroll', changeColor)
